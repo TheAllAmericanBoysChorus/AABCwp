45e80f9d1f38b66677812a04ff992d3feede5e1f
thegrid22593 <garrett.sanderson@gmail.com>
2015-03-17 11:24:03 -0700

    Updates Plugin/Panel Development

--- wp-content/themes/responsive/sidebar-home.php
+++ wp-content/themes/responsive/sidebar-home.php
@@ -63,8 +63,6 @@ if( !defined( 'ABSPATH' ) ) {
 					$the_query = new WP_Query( $args );
 
 			?>
-			<h1>Up Coming Events</h1>
-            <hr />
 			<img class="up-coming-events-img" src="<?php bloginfo( 'template_directory' )?>/core/images/L10013172.jpg"/>
 			<?php if( have_posts() ) : ?>
 
@@ -105,18 +103,116 @@ if( !defined( 'ABSPATH' ) ) {
 			<a href="taabc.org/calendar"><button class="home-calendar-btn">View Our Calendar</button></a> 
 		</div>
 
+		<?php wp_reset_query(); ?>
+
 		<div id="panel2" class="panel">
-			content2<br />
-			content2<br />
-			content2<br />
-			content2<br />
+		<h1>What Our Fans Are Saying</h1>
+			<?php
+
+					$args = array(
+						'post_type' => 'testimonial',
+						'posts_per_page' => '1',
+					);
+
+					$the_query = new WP_Query( $args );
+
+			?>
+
+			<?php if( have_posts() ) : ?>
+
+				<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+					<?php responsive_entry_before(); ?>
+					<div id="post-<?php the_ID(); ?>"<?php post_class(); ?>>
+						<?php responsive_entry_top(); ?>
+
+						<?php get_template_part( 'post-meta-page' ); ?>
+
+						<div class="post-entry">
+							<div class="testimonial-content"><?php the_field('testimonial'); ?></div>
+							<div class="testimonial-name">- <?php the_field('name'); ?></div>
+							<?php wp_link_pages( array( 'before' => '<div class="pagination">' . __( 'Pages:', 'responsive' ), 'after' => '</div>' ) ); ?>
+						</div>
+						<!-- end of .post-entry -->
+
+						
+						<?php responsive_entry_bottom(); ?>
+					</div><!-- end of #post-<?php the_ID(); ?> -->
+					<?php responsive_entry_after(); ?>
+
+					<?php responsive_comments_before(); ?>
+					<?php comments_template( '', true ); ?>
+					<?php responsive_comments_after(); ?>
+
+				<?php
+				endwhile;
+
+				get_template_part( 'loop-nav' );
+
+			else :
+
+				get_template_part( 'loop-no-posts' );
+
+			endif;
+			?>
 		</div>
 
+		<?php wp_reset_query(); ?>
+
 		<div id="panel3" class="panel">
-			content3<br />
-			content3<br />
-			content3<br />
-			content3<br />
+
+			<h1>Alumni Spotlight</h1>
+
+			<?php
+
+					$args = array(
+						'post_type' => 'aspotlight',
+						'posts_per_page' => '1',
+					);
+
+					$the_query = new WP_Query( $args );
+
+			?>
+
+			<?php if( have_posts() ) : ?>
+
+				<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+					<?php responsive_entry_before(); ?>
+					<div id="post-<?php the_ID(); ?>"<?php post_class(); ?>>
+						<?php responsive_entry_top(); ?>
+
+						<?php get_template_part( 'post-meta-page' ); ?>
+
+						<div class="post-entry">
+							<?php the_field('youtube_video'); ?>
+							<?php the_field('biography'); ?>
+							<?php wp_link_pages( array( 'before' => '<div class="pagination">' . __( 'Pages:', 'responsive' ), 'after' => '</div>' ) ); ?>
+						</div>
+						<!-- end of .post-entry -->
+
+						
+						<?php responsive_entry_bottom(); ?>
+					</div><!-- end of #post-<?php the_ID(); ?> -->
+					<?php responsive_entry_after(); ?>
+
+					<?php responsive_comments_before(); ?>
+					<?php comments_template( '', true ); ?>
+					<?php responsive_comments_after(); ?>
+
+				<?php
+				endwhile;
+
+				get_template_part( 'loop-nav' );
+
+			else :
+
+				get_template_part( 'loop-no-posts' );
+
+			endif;
+			?>
+
+
 		</div>
 	</div>
 </div> <!-- end of .col-700 fit -->
